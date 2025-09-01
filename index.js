eba79e846347271de60f82c809fdc7ca58a4cb16
Patinya-cha <patinya.cha04@gmail.com>
2025-09-01 16:20:53 +0700

    serverless

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import helmet from "helmet";
 import cors from "cors";
 import apiRoutes from "./api/v1/routes.js";
 import { connectMongo } from "./config/mongo.js";
-import { connectTurso, db } from "./config/turso.js";
+// import { connectTurso, db } from "./config/turso.js";
 import limiter from "./middleware/rateLimiter.js";
 import errorHandler from "./middleware/errorHandler.js";
 import cookieParser from "cookie-parser";
@@ -23,7 +23,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://rag-notes.vercel.app",
+    "https://ex-rag-notes-frontend.vercel.app/",
   ], // frontend domain
   credentials: true, // ✅ allow cookies to be sent
 };
@@ -33,7 +33,7 @@ app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
 // Centralized routes
-app.use("/", apiRoutes(db));
+app.use("/", apiRoutes());
 app.get("/", (_req, res) => {
   res.send(`
       <!DOCTYPE html>
@@ -89,11 +89,12 @@ const PORT = process.env.PORT || 3000;
 
 (async () => {
   try {
-    await connectMongo();
-    await connectTurso();
-    app.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT} ✅`);
-    });
+    // await connectMongo();
+    // // await connectTurso();
+    // app.listen(PORT, () => {
+    //   console.log(`Server listening on port ${PORT} ✅`);
+    // });
+    serverless(app)
   } catch (err) {
     console.error("❌ Startup error:", err);
     process.exit(1);
