1393f6a6099683ec4acf7fda4204cf245dfda391
Patinya-cha <patinya.cha04@gmail.com>
2025-09-02 13:58:28 +0700

    add https

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://ex-rag-notes-frontend.vercel.app/",
+    "https://ex-rag-notes-frontend.vercel.app",
+    "https://ex-rag-notes-frontend.vercel.app",
   ], // frontend domain
   credentials: true, // ✅ allow cookies to be sent
 };
